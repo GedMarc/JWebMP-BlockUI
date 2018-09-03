a7a49edc83670e279d53da8008c76f999d9b8889
GedMarc <ged_marc@hotmail.com>
2018-09-03 22:51:23 +0200

    Make page configuration optional, update the app

--- src/main/java/com/jwebmp/plugins/blockui/BlockUIPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/blockui/BlockUIPageConfigurator.java
@@ -10,6 +10,35 @@ public class BlockUIPageConfigurator
 		implements IPageConfigurator
 {
 	private static final JavascriptReference reference = new JavascriptReference("BlockUIReference", 2.70, "bower_components/blockui/jquery.blockUI.min.js");
+	/**
+	 * If this configurator is enabled
+	 */
+	private static boolean enabled = true;
+
+	/**
+	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static boolean isEnabled()
+	{
+		return BlockUIPageConfigurator.enabled;
+	}
+
+	/**
+	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @param mustEnable
+	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static void setEnabled(boolean mustEnable)
+	{
+		BlockUIPageConfigurator.enabled = mustEnable;
+	}
 
 	@Override
 	public @NotNull Page<?> configure(Page<?> page)
@@ -19,5 +48,11 @@ public class BlockUIPageConfigurator
 			page.addJavaScriptReference(BlockUIPageConfigurator.reference);
 		}
 		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return BlockUIPageConfigurator.enabled;
 	}
 }
