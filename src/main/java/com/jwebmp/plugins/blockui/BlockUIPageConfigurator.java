fb802b20578fbd66bb502448e334efdca006762b
GedMarc <ged_marc@hotmail.com>
2019-04-01 05:00:48 +0200

    Updates for Enterprise and ActivityMaster.

--- src/main/java/com/jwebmp/plugins/blockui/BlockUIPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/blockui/BlockUIPageConfigurator.java
@@ -2,6 +2,7 @@ package com.jwebmp.plugins.blockui;
 
 import com.jwebmp.core.Page;
 import com.jwebmp.core.base.references.JavascriptReference;
+import com.jwebmp.core.base.servlets.enumarations.RequirementsPriority;
 import com.jwebmp.core.services.IPageConfigurator;
 
 import javax.validation.constraints.NotNull;
@@ -9,7 +10,8 @@ import javax.validation.constraints.NotNull;
 public class BlockUIPageConfigurator
 		implements IPageConfigurator<BlockUIPageConfigurator>
 {
-	private static final JavascriptReference reference = new JavascriptReference("BlockUIReference", 2.70, "bower_components/blockui/jquery.blockUI.min.js");
+	private static final JavascriptReference reference = new JavascriptReference("BlockUIReference", 2.70, "bower_components/blockui/jquery.blockUI.min.js").setPriority(
+			RequirementsPriority.Fourth).setSortOrder(100);
 	/**
 	 * If this configurator is enabled
 	 */
