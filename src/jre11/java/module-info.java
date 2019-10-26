c6cdcb432092f85551f92e3d235d9129a61939ab
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:35 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -4,12 +4,12 @@ module com.jwebmp.plugins.blockui {
 
 	requires com.jwebmp.core;
 	requires java.validation;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.blockui.BlockUIPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.blockui.implementations.JQBlockUIExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.blockui.implementations.JQBlockUIExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.blockui.implementations.JQBlockUIExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.blockui.implementations.JQBlockUIExclusionsModule;
 
 	opens com.jwebmp.plugins.blockui.options to com.jwebmp.core, com.google.inject, com.fasterxml.jackson.core;
 }
