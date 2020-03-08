6146dd4517cc1639aadcc2e5673a64d4ea0ca962
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:08 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -9,7 +9,6 @@ module com.jwebmp.plugins.blockui {
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.blockui.BlockUIPageConfigurator;
 
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.blockui.implementations.JQBlockUIExclusionsModule;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.blockui.implementations.JQBlockUIExclusionsModule;
 
 	opens com.jwebmp.plugins.blockui.options to com.jwebmp.core, com.google.inject, com.fasterxml.jackson.core;
 }
--- src/main/java/com/jwebmp/plugins/blockui/implementations/JQBlockUIExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/blockui/implementations/JQBlockUIExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.blockui.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class JQBlockUIExclusionsModule
-		implements IGuiceScanModuleExclusions<JQBlockUIExclusionsModule>,
-				           IGuiceScanJarExclusions<JQBlockUIExclusionsModule>
+		implements IGuiceScanModuleExclusions<JQBlockUIExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-block-ui-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
